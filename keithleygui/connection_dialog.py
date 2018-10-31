7d41dcd69ddadcc17a9eb0ec92afdeeeebb9a52c
Sam Schott <ss2151@cam.ac.uk>
2018-10-31 16:25:43 +0000

    split off ConnectionDialog into separate file

---
+++ keithleygui/connection_dialog.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 16:23:13 2018
+
+@author: samschott
+"""
+
+from __future__ import division, print_function, absolute_import
+import os.path as osp
+import pkg_resources as pkgr
+import visa
+import pyvisa
+from qtpy import QtCore, QtWidgets, uic
+
+# local imports
+from keithleygui.config.main import CONF
+
+CONNECTION_UI_PATH = pkgr.resource_filename('keithleygui', 'connection_dialog.ui')
+
+
+class ConnectionDialog(QtWidgets.QDialog):
+
+    def __init__(self, instr):
+        super(self.__class__, self).__init__()
+        # load user interface layout from .ui file
+        uic.loadUi(CONNECTION_UI_PATH, self)
+
+        self.instr = instr
+
+        # populate UI
+        self.populate_ui_from_instr()
+
+        # connect callbacks
+        self.checkBoxAutoVisa.clicked.connect(self._on_auto_checked)
+        self.buttonBox.accepted.connect(self._on_accept)
+        self.buttonBox.rejected.connect(self.populate_ui_from_instr)
+        self.buttonBox.helpRequested.connect(self._on_help_clicked)
+        self.pushButtonChoose.clicked.connect(self._on_choose_clicked)
+        self.pushButtonSearch.clicked.connect(self._on_search_clicked)
+
+    @QtCore.Slot()
+    def populate_ui_from_instr(self):
+        is_auto = self.instr.visa_library == ''
+        self.checkBoxAutoVisa.setChecked(is_auto)
+        self._on_auto_checked(is_auto)
+        self._on_search_clicked()  # search for instrument addresses
+
+    @QtCore.Slot(bool)
+    def _on_auto_checked(self, checked):
+        """Switch from automatic to manual visa library selection."""
+        if checked:
+            self.labelVisaLib.close()
+            self.lineEditLibrary.close()
+            self.pushButtonChoose.close()
+            self.lineEditLibrary.setText(self.instr.rm.visalib.library_path)
+        else:
+            self.labelVisaLib.show()
+            self.lineEditLibrary.show()
+            self.pushButtonChoose.show()
+            self.lineEditLibrary.setText(self.instr.visa_library)
+
+    @QtCore.Slot()
+    def _on_choose_clicked(self):
+        """Select path to VISA library."""
+        prompt = 'Please select a DLL.'
+        filepath = QtWidgets.QFileDialog.getOpenFileName(self, prompt)
+        if not osp.isfile(filepath[0]):
+            return
+        self.lineEditLibrary.setText(filepath[0])
+
+    @QtCore.Slot()
+    def _on_search_clicked(self):
+        # set Address comboBox status
+        self.comboBoxAddress.clear()
+        self.comboBoxAddress.addItems([self.instr.visa_address])
+        self.comboBoxAddress.addItems(self.instr.rm.list_resources())
+        self.comboBoxAddress.setCurrentIndex(0)
+
+    @QtCore.Slot()
+    def _on_accept(self):
+        """ Update connection settings, reconnect with new settings."""
+        self.instr.visa_library = self.lineEditLibrary.text()
+        self.instr.visa_address = self.comboBoxAddress.currentText()
+
+        CONF.set('Connection', 'VISA_LIBRARY', self.instr.visa_library)
+        CONF.set('Connection', 'VISA_ADDRESS', self.instr.visa_address)
+
+        # reconnect with new address
+        # close and reopen ResourceManager for visa_lib path change to take effect
+        # TODO: this needs testing
+        self.instr.disconnect()
+        self.instr.rm.close()
+
+        try:
+            self.instr.rm = visa.ResourceManager(self.instr.visa_library)
+
+        except ValueError:
+            msg = ('Could not find backend %s.\n' % self.lineEditLibrary.text() +
+                   'Using default backend instead.')
+            QtWidgets.QMessageBox.information(self, str('error'), msg)
+
+            self.instr.visa_library = ''
+            self.instr.rm = visa.ResourceManager()
+
+            self.populate_ui_from_instr()
+
+        self.instr.connect()
+
+    @QtCore.Slot()
+    def _on_help_clicked(self):
+        """Show dialog box with help."""
+
+        ni_visa_link = 'https://www.ni.com/visa/'
+
+        msg = """
+        <p>If "%s" is selectd, NI-VISA will be automatically detected and used. If NI-VISA is not
+        installed, pyvisa-py we will be used as a fallback. Alternatively, you may manually select
+        the NI-VISA library path or choose a pyvisa-py backend by entering "@py" in the path
+        field.</p>
+
+        <p>All detected visa instruments will be listed, but automatic detection may not work with
+        instruments connected via Ethernet or on remote computers.</p>
+
+        <p>You can get NI-VISA from <a href="%s"> %s</a>.</p>
+
+        <p><b>PyVisa detected the following setup:</b></p>
+        """ % (self.checkBoxAutoVisa.text(), ni_visa_link, ni_visa_link)
+
+        visa_info = pyvisa.util.get_debug_info(to_screen=False)
+        visa_info = '<p style="white-space: pre-wrap;">' + visa_info + '</p>'
+
+        self.helpText = QtWidgets.QTextBrowser()
+        self.helpText.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.helpText.setHtml(msg + visa_info)
+        self.helpText.resize(500, 400)
+        self.helpText.show()
--- keithleygui/main.py
+++ keithleygui/main.py
@@ -8,7 +8,7 @@ Created on Tue Feb 20 15:01:18 2018
 
 # system imports
 from __future__ import division, print_function, absolute_import
-import os.path as ops
+import os.path as osp
 import pkg_resources as pkgr
 import visa
 from qtpy import QtGui, QtCore, QtWidgets, uic
@@ -21,11 +21,11 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from keithleygui.utils.led_indicator_widget import LedIndicator
 from keithleygui.utils.scientific_spinbox import ScienDSpinBox
 from keithleygui.config.main import CONF
+from keithleygui.connection_dialog import ConnectionDialog
 
 
 MAIN_UI_PATH = pkgr.resource_filename('keithleygui', 'main.ui')
 MPL_STYLE_PATH = pkgr.resource_filename('keithleygui', 'figure_style.mplstyle')
-CONNECTION_UI_PATH = pkgr.resource_filename('keithleygui', 'connection_dialog.ui')
 
 
 class KeithleyGuiApp(QtWidgets.QMainWindow):
@@ -229,7 +229,7 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
         if self.keithley.busy:
             msg = ('Keithley is currently used by antoher program. ' +
                    'Please try again later.')
-            QtWidgets.QMessageBox.information(None, str('error'), msg)
+            QtWidgets.QMessageBox.information(self, str('error'), msg)
 
 # =============================================================================
 # Measurement callbacks
@@ -366,7 +366,7 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
             msg = ('Keithley cannot be reached at %s. ' % self.keithley.visa_address
                    + 'Please check if address is correct and Keithley is ' +
                    'turned on.')
-            QtWidgets.QMessageBox.information(None, str('error'), msg)
+            QtWidgets.QMessageBox.information(self, str('error'), msg)
 
     @QtCore.Slot()
     def _on_disconnect_clicked(self):
@@ -394,7 +394,7 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
         """Show GUI to load sweep data from file."""
         prompt = 'Please select a data file.'
         filepath = QtWidgets.QFileDialog.getOpenFileName(self, prompt)
-        if not ops.isfile(filepath[0]):
+        if not osp.isfile(filepath[0]):
             return
         try:
             self.sweepData = TransistorSweepData()
@@ -486,7 +486,7 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
         except ValueError:
             idx_sweep = 0
             msg = 'Could not find last used SMUs in Keithley driver.'
-            QtWidgets.QMessageBox.information(None, str('error'), msg)
+            QtWidgets.QMessageBox.information(self, str('error'), msg)
 
         self.comboBoxGateSMU.setCurrentIndex(idx_sweep)
 
@@ -518,7 +518,7 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
             self.comboBoxGateSMU.setCurrentIndex(0)
             self.comboBoxDrainSMU.setCurrentIndex(1)
             msg = 'Could not find last used SMUs in Keithley driver.'
-            QtWidgets.QMessageBox.information(None, str('error'), msg)
+            QtWidgets.QMessageBox.information(self, str('error'), msg)
 
         for i in range(0, self.ntabs):
             sense = CONF.get(self.smu_list[i], 'sense')
@@ -573,7 +573,7 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
         self.pushButtonTransfer.setEnabled(True)
         self.pushButtonOutput.setEnabled(True)
         self.pushButtonIV.setEnabled(True)
-        self.pushButtonAbort.setEnabled(True)
+        self.pushButtonAbort.setEnabled(False)
 
         self.actionConnect.setEnabled(False)
         self.actionDisconnect.setEnabled(True)
@@ -645,62 +645,6 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
             self.canvas.draw()
 
 
-class ConnectionDialog(QtWidgets.QDialog):
-
-    def __init__(self, instrument):
-        super(self.__class__, self).__init__()
-        # load user interface layout from .ui file
-        uic.loadUi(CONNECTION_UI_PATH, self)
-
-        self.instrument = instrument
-        self.lineEditLibrary.setText(self.instrument.visa_library)
-        self._on_search_clicked()  # search for instrument addresses
-
-        self.buttonBox.accepted.connect(self._on_accept)
-        self.buttonBox.rejected.connect(self._on_reject)
-        self.pushButtonChoose.clicked.connect(self._on_choose_clicked)
-        self.pushButtonSearch.clicked.connect(self._on_search_clicked)
-
-    @QtCore.Slot()
-    def _on_choose_clicked(self):
-        """Select path to VISA library."""
-        prompt = 'Please select a DLL.'
-        filepath = QtWidgets.QFileDialog.getOpenFileName(self, prompt)
-        if not ops.isfile(filepath[0]):
-            return
-        self.lineEditLibrary.setText(filepath[0])
-
-    @QtCore.Slot()
-    def _on_search_clicked(self):
-        # set Address comboBox status
-        self.comboBoxAddress.clear()
-        self.comboBoxAddress.addItems([self.instrument.visa_address])
-        self.comboBoxAddress.addItems(self.instrument.rm.list_resources())
-        self.comboBoxAddress.setCurrentIndex(0)
-
-    @QtCore.Slot()
-    def _on_accept(self):
-        """ Update connection settings, reconnect with new settings."""
-        self.instrument.visa_library = self.lineEditLibrary.text()
-        self.instrument.visa_address = self.comboBoxAddress.currentText()
-
-        CONF.set('Connection', 'VISA_LIBRARY', self.instrument.visa_library)
-        CONF.set('Connection', 'KEITHLEY_ADDRESS', self.instrument.visa_address)
-
-        # reconnect with new Library
-        # TODO: this needs testing
-        self.instrument.disconnect()
-        self.instrument.rm.close()
-        self.instrument.rm = visa.ResourceManager(self.instrument.visa_library)
-        self.instrument.connect()
-
-    @QtCore.Slot()
-    def _on_reject(self):
-        """ Discard user input to text fields."""
-        self.lineEditLibrary.setText(self.instrument.visa_library)
-        self._on_search_clicked()  # search for instrument addresses
-
-
 class MeasureThread(QtCore.QThread):
 
     startedSig = QtCore.Signal()
@@ -749,7 +693,7 @@ def run():
     import sys
     from keithley2600 import Keithley2600
 
-    KEITHLEY_ADDRESS = CONF.get('Connection', 'KEITHLEY_ADDRESS')
+    KEITHLEY_ADDRESS = CONF.get('Connection', 'VISA_ADDRESS')
     VISA_LIBRARY = CONF.get('Connection', 'VISA_LIBRARY')
     keithley = Keithley2600(KEITHLEY_ADDRESS, VISA_LIBRARY)
 
