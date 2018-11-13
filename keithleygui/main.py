24ae173f5a37c3c939ff633d13c717bb89348983
Sam Schott <ss2151@cam.ac.uk>
2018-11-13 19:17:13 +0000

    bug fix

--- keithleygui/main.py
+++ keithleygui/main.py
@@ -83,8 +83,12 @@ class KeithleyGuiApp(QtWidgets.QMainWindow):
 # =============================================================================
 
     def restoreGeometry(self):
-        self.move(CONF.get('Window', 'x'), CONF.get('Window', 'y'))
-        self.resize(CONF.get('Window', 'width'), CONF.get('Window', 'height'))
+        x = CONF.get('Window', 'x')
+        y = CONF.get('Window', 'y')
+        w = CONF.get('Window', 'width')
+        h = CONF.get('Window', 'height')
+
+        self.setGeometry(x, y, w, h)
 
     def saveGeometry(self):
         geo = self.geometry()
