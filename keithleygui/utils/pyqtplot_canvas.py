4da6da9f5b4cf0d8460662db3b852cdfd764303b
Sam Schott <ss2151@cam.ac.uk>
2019-06-18 10:54:15 +0100

    move legend code to pyqtgraph_cx

--- keithleygui/utils/pyqtplot_canvas.py
+++ keithleygui/utils/pyqtplot_canvas.py
@@ -19,136 +19,6 @@ pg.setConfigOptions(antialias=True, exitCleanup=False)
 
 
 # ========================================================================================
-# Create our own, more beautiful, legend here.
-# ========================================================================================
-
-class MySampleItem(GraphicsWidget):
-    """ Class responsible for drawing a single item in a LegendItem (sans label).
-    """
-    # TODO: make this more generic; let each item decide how it should be represented.
-    def __init__(self, item):
-        GraphicsWidget.__init__(self)
-        self.item = item
-
-    def boundingRect(self):
-        return QtCore.QRectF(0, 0, 20, 20)
-
-    def paint(self, p, *args):
-        opts = self.item.opts
-
-        if opts['antialias']:
-            p.setRenderHint(p.Antialiasing)
-
-        if not isinstance(self.item, ScatterPlotItem):
-            p.setPen(fn.mkPen(opts['pen']))
-            p.drawLine(-7, 11, 7, 11)
-
-        symbol = opts.get('symbol', None)
-        if symbol is not None:
-            if isinstance(self.item, PlotDataItem):
-                opts = self.item.scatter.opts
-
-            pen = fn.mkPen(opts['pen'])
-            brush = fn.mkBrush(opts['brush'])
-            size = opts['size']
-
-            p.translate(10, 10)
-            path = drawSymbol(p, symbol, size, pen, brush)
-
-
-class MyLegendItem(LegendItem):
-
-    def __init__(self, size=None, offset=None, pen=None, brush=None, **kwargs):
-        LegendItem.__init__(self, size, offset)
-
-        self.layout.setSpacing(0)
-        self.layout.setColumnSpacing(0, 10)
-        self.layout.setContentsMargins(20, 0, 0, 0)
-
-        self.opts = {
-            'pen': fn.mkPen(pen),
-            'brush': brush,
-            'labelTextColor': None,
-            'box': False,
-        }
-
-        self.opts.update(kwargs)
-
-    def setOffset(self, offset):
-        self.offset = offset
-
-        offset = Point(self.offset)
-        anchorx = 1 if offset[0] <= 0 else 0
-        anchory = 1 if offset[1] <= 0 else 0
-        anchor = (anchorx, anchory)
-        self.anchor(itemPos=anchor, parentPos=anchor, offset=offset)
-
-    def addItem(self, item, name):
-        """
-        Add a new entry to the legend.
-
-        ==============  ========================================================
-        **Arguments:**
-        item            A PlotDataItem from which the line and point style
-                        of the item will be determined or an instance of
-                        ItemSample (or a subclass), allowing the item display
-                        to be customized.
-        title           The title to display for this item. Simple HTML allowed.
-        ==============  ========================================================
-        """
-        label = LabelItem(name, color=self.opts['labelTextColor'], justify='left')
-        if isinstance(item, MySampleItem):
-            sample = item
-        else:
-            sample = MySampleItem(item)
-        row = self.layout.rowCount()
-        self.items.append((sample, label))
-        self.layout.addItem(sample, row, 0)
-        self.layout.addItem(label, row, 1)
-        height = max(sample.minimumHeight(), label.minimumHeight())
-        self.layout.setRowMaximumHeight(row, height)
-        self.layout.setColumnSpacing(0, 10)
-        self.updateSize()
-
-    def setPen(self, *args, **kargs):
-        """
-        Sets the pen used to draw lines between points.
-        *pen* can be a QPen or any argument accepted by
-        :func:`pyqtgraph.mkPen() <pyqtgraph.mkPen>`
-        """
-        pen = fn.mkPen(*args, **kargs)
-        self.opts['pen'] = pen
-
-    def setBrush(self, *args, **kargs):
-        brush = fn.mkBrush(*args, **kargs)
-        if self.opts['brush'] == brush:
-            return
-        self.opts['brush'] = brush
-
-    def setLabelTextColor(self, *args, **kargs):
-        """
-        Sets the color of the label text.
-        *pen* can be a QPen or any argument accepted by
-        :func:`pyqtgraph.mkColor() <pyqtgraph.mkPen>`
-        """
-        self.opts['labelTextColor'] = fn.mkColor(*args, **kargs)
-        for sample, label in self.items:
-            label.setAttr('color', self.opts['labelTextColor'])
-
-    def clear(self):
-        for sample, label in self.items:
-            self.layout.removeItem(sample)
-            self.layout.removeItem(label)
-        while len(self.items) > 0:
-            self.items.pop()
-
-    def paint(self, p, *args):
-        p.setPen(self.opts['pen'])
-        p.setBrush(self.opts['brush'])
-        p.drawRect(self.boundingRect())
-
-
-# ========================================================================================
 # The actual plot item
 # ========================================================================================
 
@@ -235,9 +105,8 @@ class SweepDataPlot(GraphicsView):
         self.p.setYRange(-10, 10)
 
         # add legend
-        self.legend = MyLegendItem(brush=fn.mkBrush(255, 255, 255, 150),
-                                   labelTextColor='k',
-                                   offset=(20, -20))
+        self.legend = LegendItem(brush=fn.mkBrush(255, 255, 255, 150),
+                                 labelTextColor='k', offset=(20, -20))
         self.legend.setParentItem(self.p.vb)
 
     def clear(self):
@@ -290,7 +159,7 @@ class SweepDataPlot(GraphicsView):
         if text is None:
             self.p.setTitle(None)  # clears title and hides title column
         else:
-            self.p.setTitle('')  # makes title comlumn visible, sets placeholder text
+            self.p.setTitle('')  # makes title column visible, sets placeholder text
             self.p.titleLabel.item.setPlainText(text)  # replace HTML with plain text
 
         if color is not None:
