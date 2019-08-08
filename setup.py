5470651b558f8ad7d0440a13c09bae7ac2b8bc15
Sam Schott <ss2151@cam.ac.uk>
2019-08-08 14:47:45 +0100

    added classifiers

--- setup.py
+++ setup.py
@@ -34,12 +34,12 @@ setup(
     zip_safe=False,
     keywords='keithleygui',
     classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    test_suite='tests',
-    tests_require=[
-    ]
 )
