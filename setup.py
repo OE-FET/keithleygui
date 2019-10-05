f6fa3d6880c1ea3ee484199a4d72a52dd9fd6fc7
Sam Schott <ss2151@cam.ac.uk>
2019-10-06 00:09:53 +0100

    bumped pyqtgraph_cx requirement

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'keithley2600>=1.2.1',
         'numpy',
         'pyvisa',
-        'pyqtgraph_cx>=0.12',
+        'pyqtgraph_cx>=0.12.1',
         'qtpy',
         'repr',
         'setuptools',
